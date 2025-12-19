f9d4b2746d628a151899ed47cd0c25a097ba67b7
244g1a05z8-cell <244g1a05z8@srit.ac.in>
2025-12-19 15:47:50 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes{
+ static int i;
+ static char c;
+ static float f;
+ static double d;
+ static long l;
+ static short s;
+ static byte b;
+ static boolean bool;
+public static void main(String[]args){
+System.out.println("Default primitive Datatypes:");
+System.out.println("int:"+i);
+System.out.println("char:"+c);
+System.out.println("float:"+f);
+System.out.println("double:"+d);
+System.out.println("long:"+l);
+System.out.println("short:"+s);
+System.out.println("byte:"+b);
+System.out.println("boolean:"+bool);
+}
+}
+
